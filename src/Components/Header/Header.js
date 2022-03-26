756dca48343ba41d4638384cd40b3f8c24b93601
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-03-26 23:04:14 +0600

    second

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>My gun collection</h1>
+            <h4>Select any four</h4>
+        </div>
+    );
+};
+
+export default Header;
